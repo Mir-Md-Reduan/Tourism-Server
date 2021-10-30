190ebf5f84904422195eb102aec389f1ff96a8c8
Mir-Md-Reduan <mhoqu091050@bsee.uiu.ac.bd>
2021-10-30 22:12:27 +0600

    Fake data load on DataBase

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
-
-const uri = "mongodb+srv://<username>:<password>@cluster0.qdqaw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+require('dotenv').config();
 
 
-async function run() {
-    try {
-        console.log('Data Base Connected');
-    }
-    finally {
-        await client.close();
-    }
-}
-run().catch(console.dir);
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -23,6 +11,33 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qdqaw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://TorismPlaces:${process.env.DB_PASS}@cluster0.qdqaw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+
+        const database = client.db("Torism");
+        const userCollection = database.collection("Places");
+        console.log("db Connected")
+        app.get('/places', async (req, res) => {
+            const cursor = userCollection.find({});
+            const places = await cursor.toArray();
+            res.send(places);
+        })
+
+
+
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello From Assignment 11');
